204f412735b61cceea5a2d89e8088735150b29dc
IrimiaMarco <irimiamarco00@gmail.com>
2023-11-07 22:13:55 -0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Car
+    {
+        public string color;
+        public string name;
+        public int year;
+        public int maxSpeed;
+        public Car(string nameC, string colorC, int yearC, int maxC)
+        {
+            name = nameC;
+            color = colorC;
+            year = yearC;
+            maxSpeed = maxC;
+
+        }
+
+        public Car (string nameC)
+        {
+            name = nameC;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(year);
+            Console.WriteLine(color);
+            Console.WriteLine(maxSpeed);
+            fullThrottle();
+            Console.WriteLine();
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Dacia = new Car("Logan");
+            Ford.Display();
+            Opel.Display();
+            Console.WriteLine(Dacia.name);
+            Console.ReadKey();
+        }
+    }
+}
